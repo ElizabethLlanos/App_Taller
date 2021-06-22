8f77ceb7047e9e87f965dcd370af9cb720f00628
ElizabethLlanos <85859478+ElizabethLlanos@users.noreply.github.com>
2021-06-22 16:32:44 -0500

    App4_Taller_Mecánico
    Esta aplicación controla un taller mecánico para la entrada, cobros y salidas de autos, los autos se acomodan en el orden en que van llegando. El taller contiene una entrada y una salida.

---
+++ App_TallerM/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+
+namespace App_TallerM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se utilizó un objeto de la clase consoleKeyInfo para el menú
+            ConsoleKeyInfo menu = new ConsoleKeyInfo();
+            //Instancia
+            TallerMecanico objTallerM = new TallerMecanico();
+            double Cobro;
+            objTallerM.CAPACIDAD = 0;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Elige una opción: ");
+                Console.WriteLine("a) Ingresar Auto");
+                Console.WriteLine("b) Aplicar cobro y sacar Auto");
+                Console.WriteLine("c) Mostrar Lista de Autos");
+                Console.WriteLine("d) Salir");
+                menu = Console.ReadKey(true);
+                switch (menu.Key)
+                {
+                    case ConsoleKey.A:
+                        {
+                            Console.Clear();
+                            //Este if nos evitará exeder de la capacidad del taller
+                            if (objTallerM.CAPACIDAD > 4)
+                            {
+                                Console.WriteLine("¡El taller se encuentra lleno!");
+                                Console.ReadKey(); break;
+                            }
+                            Console.WriteLine("♦♦♦ REGISTRO ♦♦♦");
+                            Console.Write("►►Dueño: ");
+                            //Se añade en la cola
+                            objTallerM.Dueños.Enqueue(Console.ReadLine());
+                            Console.Write("►►Placas: ");
+                            //Se añade en la cola
+                            objTallerM.Autos.Enqueue(Console.ReadLine());
+                            //Se aumenta 1 a la capacidad y a los autos atendidos
+                            objTallerM.CAPACIDAD++;
+                            objTallerM.AUTOS_ATENDIDOS++;
+                        }
+                        break;
+                    case ConsoleKey.B:
+                        {
+                            Console.Clear();
+                            if (objTallerM.CAPACIDAD == 0)
+                            {
+                                Console.WriteLine("♦ EL TALLER ESTÁ VACIO ♦");
+                                Console.ReadKey(); break;
+                            }
+                            Console.Write("►►COBRO: ");
+                            Cobro = Convert.ToDouble(Console.ReadLine());
+                            objTallerM.COBRO = objTallerM.COBRO + Cobro;
+                            //Se agregan a la lista de los cobros
+                            objTallerM.Lista_Cobros.Add(Cobro);
+                            //Agregar a la lista de autos atendidos y sacar del taller.
+                            objTallerM.Lista_Autos.Add(objTallerM.Autos.Dequeue());
+                            //Se agrega a la lista propietarios
+                            objTallerM.Lista_Dueños.Add(objTallerM.Dueños.Dequeue());
+                            //Se disminuye a la capacidad para poder ingresar otro auto
+                            objTallerM.CAPACIDAD--;
+                        }
+                        break;
+                    case ConsoleKey.C:
+                        {
+                            Console.Clear();
+                            //Se muestra el total de los autos atendidos
+                            Console.WriteLine($"AUTOS ATENDIDOS: {objTallerM.AUTOS_ATENDIDOS}");
+                            for (int i = 0; i < objTallerM.AUTOS_ATENDIDOS; i++)
+                            {
+                                Console.WriteLine("************************************");
+                                Console.WriteLine($"►Placas: {objTallerM.Lista_Autos[i]}");
+                                Console.WriteLine($"►Propietario: {objTallerM.Lista_Dueños[i]}");
+                                Console.WriteLine($"►Cobro: {objTallerM.Lista_Cobros[i]}");
+                                Console.WriteLine();
+                            }
+                            Console.WriteLine("*******************************************");
+                            //Se muestra el total de lo que se cobró en todo el día
+                            Console.WriteLine($"Cantidad total: $ {objTallerM.COBRO}");
+                            Console.ReadKey();
+                        }
+                        break;
+                }
+            } while (menu.Key != ConsoleKey.D);
+
+        }
+    }
+}
---
+++ App_TallerM/TallerMecanico.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace App_TallerM
+{
+    class TallerMecanico
+    {
+        ConsoleKeyInfo Opcion = new ConsoleKeyInfo();
+        //Cola para dueños
+        public Queue<string> Dueños = new Queue<string>();
+        //Cola para autos
+        public Queue<string> Autos = new Queue<string>();
+        //Listas
+        public ArrayList Lista_Dueños = new ArrayList();
+        public ArrayList Lista_Cobros = new ArrayList();
+        public ArrayList Lista_Autos = new ArrayList();
+       
+        //-----Atributos
+        int capacidad, autos_atendidos;
+        double cobro = 0;
+        //----Propiedades
+        public int CAPACIDAD
+        {
+            get { return this.capacidad; }
+            set { this.capacidad = value; }
+        }
+        public double COBRO
+        {
+            get { return this.cobro; }
+            set { this.cobro = value; }
+        }
+        public int AUTOS_ATENDIDOS
+        {
+            get { return this.autos_atendidos; }
+            set { this.autos_atendidos = value; }
+        }
+        
+    }
+}
